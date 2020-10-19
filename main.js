232196cdc3c9e49ac7cc9b22da311e4937b4d154
Malek Farhat <bannour_faiza@hotmail.fr>
2020-10-19 16:56:07 +0100

    modification

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var clicked=false;
 $('.types').click(function(){
     if(!clicked){
     $('.item3').append('<p>The first step is to determine which visa type you need, <br> and check whether you’re eligible to apply for it. <br> You’ll also need to know the documents that you’ll have to submit along with your application, <br> how long the application might take and fees you’ll have to pay.</p>').css('text-align','left');
+    $('.item3').append('<FORM><SELECT><OPTION> Please select visa type! <OPTION> Business <OPTION> Visit Family/Friend <OPTION> Tourist <OPTION> Study <OPTION> Trade Fair <OPTION> Medical </SELECT></FORM>');
+    $('.item3').append('<FORM><INPUT type="button" name="nom" value= "Check List"></FORM>');
+
     clicked=true;
 }
 });
